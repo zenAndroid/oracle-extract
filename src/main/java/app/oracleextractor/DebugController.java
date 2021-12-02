4b0779383b909f588a519042ed32022171bb1e12
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2021-12-02 18:01:15 -0500

    Move the default machine factory method to Machine.java.

--- src/main/java/app/oracleextractor/DebugController.java
+++ src/main/java/app/oracleextractor/DebugController.java
@@ -1,8 +1,6 @@
 package app.oracleextractor;
 
 import app.oracleextractor.model.Machine;
-import app.oracleextractor.model.State;
-import app.oracleextractor.model.Transition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -20,7 +18,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-import java.util.Set;
 
 public class DebugController implements Initializable {
     @FXML
@@ -48,7 +45,7 @@ public class DebugController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initializeUIElements();
-        machineOfInterest = getDefaultMachine();
+        machineOfInterest = Machine.getDefaultMachine();
         cloneOfInterest = machineOfInterest.makeMachineCopy();
         updateMachineView(machineOfInterest);
         // 1 2 2 2 2 1
@@ -109,78 +106,6 @@ public class DebugController implements Initializable {
         return retVal;
     }
 
-    /**
-     * @return A default machine, hardcoded and used for testing purposes.
-     */
-    public Machine getDefaultMachine() {
-
-        Machine debug = new Machine();
-
-        // digraph Automaton {
-        //     // labelloc = "t"
-        //     // label = "Consumed input: baaa\nProduced output: 2212"
-        //     INIT [shape=point];
-        //     s1 [shape="doublecircle"];
-        //     node [shape=circle];
-        //     rankdir = LR;
-        //     INIT -> s1;
-        //     s1 -> s2 [label="b/2", color=red];
-        //     s1 -> s4 [label="a/1"];
-        //     s2 -> s1 [label="a/2"];
-        //     s2 -> s3 [label="b/2"];
-        //     s3 -> s2 [label="a/1"];
-        //     s3 -> s1 [label="b/2"];
-        //     s4 -> s1 [label="b/1"];
-        //     s4 -> s3 [label="a/2"];
-        // }
-
-
-        // 1- States.
-        // 2- Transitions.
-        // 3- Giving states their transitions.
-        // 4- Set the initial state.
-        // 5- Set the input alphabet (Still sus)
-        // 6- Set the output alphabet (still sus)
-        // 7- Set the machine states
-        // 8- Set the input sequence.
-        // 9- CONSUME !!!
-
-        /*
-        The state are created, with their uniques names;
-         */
-        State s1 = new State();
-        State s2 = new State();
-        State s3 = new State();
-        State s4 = new State();
-        /*
-        The transitions are created with their trigger, output, source states and destination states.
-         */
-        Transition t1 = new Transition('b', '2', s1, s2);
-        Transition tnd2 = new Transition('b', '2', s1, s4);
-        Transition t2 = new Transition('a', '1', s1, s4);
-        Transition t3 = new Transition('a', '2', s2, s1);
-        Transition t4 = new Transition('b', '2', s2, s3);
-        Transition t5 = new Transition('a', '1', s3, s2);
-        Transition t6 = new Transition('b', '2', s3, s1);
-        Transition t7 = new Transition('b', '1', s4, s1);
-        Transition t8 = new Transition('a', '2', s4, s3);
-        Transition tnd9 = new Transition('a', '2', s4, s2); // Non-determinism test
-
-        s1.setStateTransitions(t1, t2, tnd2);
-        s2.setStateTransitions(t3, t4);
-        s3.setStateTransitions(t5, t6);
-        s4.setStateTransitions(t7, t8, tnd9);
-
-        debug.setInitialState(s1);
-
-        debug.setInputAlphabet(Set.of('a', 'b'));
-
-        debug.setOutputAlphabet(Set.of('1', '2'));
-
-        debug.setStates(s1, s2, s3, s4);
-
-        return debug;
-    }
 
     /**
      * @param argMachine The machine that will be displayed on the webView.
--- src/main/java/app/oracleextractor/model/Machine.java
+++ src/main/java/app/oracleextractor/model/Machine.java
@@ -95,6 +95,80 @@ public class Machine {
         producedOutput = new ArrayList<>();
     }
 
+    /**
+     * @return A default machine, hardcoded and used for testing purposes.
+     */
+    public static Machine getDefaultMachine() {
+
+        Machine debug = new Machine();
+
+        // digraph Automaton {
+        //     // labelloc = "t"
+        //     // label = "Consumed input: baaa\nProduced output: 2212"
+        //     INIT [shape=point];
+        //     s1 [shape="doublecircle"];
+        //     node [shape=circle];
+        //     rankdir = LR;
+        //     INIT -> s1;
+        //     s1 -> s2 [label="b/2", color=red];
+        //     s1 -> s4 [label="a/1"];
+        //     s2 -> s1 [label="a/2"];
+        //     s2 -> s3 [label="b/2"];
+        //     s3 -> s2 [label="a/1"];
+        //     s3 -> s1 [label="b/2"];
+        //     s4 -> s1 [label="b/1"];
+        //     s4 -> s3 [label="a/2"];
+        // }
+
+
+        // 1- States.
+        // 2- Transitions.
+        // 3- Giving states their transitions.
+        // 4- Set the initial state.
+        // 5- Set the input alphabet (Still sus)
+        // 6- Set the output alphabet (still sus)
+        // 7- Set the machine states
+        // 8- Set the input sequence.
+        // 9- CONSUME !!!
+
+        /*
+        The state are created, with their uniques names;
+         */
+        State s1 = new State();
+        State s2 = new State();
+        State s3 = new State();
+        State s4 = new State();
+        /*
+        The transitions are created with their trigger, output, source states and destination states.
+         */
+        Transition t1 = new Transition('b', '2', s1, s2);
+        Transition tnd2 = new Transition('b', '2', s1, s4);
+        Transition t2 = new Transition('a', '1', s1, s4);
+        Transition t3 = new Transition('a', '2', s2, s1);
+        Transition t4 = new Transition('b', '2', s2, s3);
+        Transition t5 = new Transition('a', '1', s3, s2);
+        Transition t6 = new Transition('b', '2', s3, s1);
+        Transition t7 = new Transition('b', '1', s4, s1);
+        Transition t8 = new Transition('a', '2', s4, s3);
+        Transition tnd9 = new Transition('a', '2', s4, s2); // Non-determinism test
+
+        s1.setStateTransitions(t1, t2, tnd2);
+        s2.setStateTransitions(t3, t4);
+        s3.setStateTransitions(t5, t6);
+        s4.setStateTransitions(t7, t8, tnd9);
+
+        debug.setInitialState(s1);
+
+        debug.setInputAlphabet(Set.of('a', 'b'));
+
+        debug.setOutputAlphabet(Set.of('1', '2'));
+
+        debug.setStates(s1, s2, s3, s4);
+
+        return debug;
+    }
+
+
     public static void main(String[] args) {
         // Creating the states;
 

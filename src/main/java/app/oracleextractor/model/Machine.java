b7d195f3019f0f4b684fb7800af288ab29a6e783
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2021-12-02 18:20:32 -0500

    Remove bloat comment, modify some transitions temporarily to detect if the random chooser works, modify main.

--- src/main/java/app/oracleextractor/model/Machine.java
+++ src/main/java/app/oracleextractor/model/Machine.java
@@ -1,7 +1,6 @@
 package app.oracleextractor.model;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
@@ -102,35 +101,6 @@ public class Machine {
 
         Machine debug = new Machine();
 
-        // digraph Automaton {
-        //     // labelloc = "t"
-        //     // label = "Consumed input: baaa\nProduced output: 2212"
-        //     INIT [shape=point];
-        //     s1 [shape="doublecircle"];
-        //     node [shape=circle];
-        //     rankdir = LR;
-        //     INIT -> s1;
-        //     s1 -> s2 [label="b/2", color=red];
-        //     s1 -> s4 [label="a/1"];
-        //     s2 -> s1 [label="a/2"];
-        //     s2 -> s3 [label="b/2"];
-        //     s3 -> s2 [label="a/1"];
-        //     s3 -> s1 [label="b/2"];
-        //     s4 -> s1 [label="b/1"];
-        //     s4 -> s3 [label="a/2"];
-        // }
-
-
-        // 1- States.
-        // 2- Transitions.
-        // 3- Giving states their transitions.
-        // 4- Set the initial state.
-        // 5- Set the input alphabet (Still sus)
-        // 6- Set the output alphabet (still sus)
-        // 7- Set the machine states
-        // 8- Set the input sequence.
-        // 9- CONSUME !!!
-
         /*
         The state are created, with their uniques names;
          */
@@ -142,9 +112,9 @@ public class Machine {
         The transitions are created with their trigger, output, source states and destination states.
          */
         Transition t1 = new Transition('b', '2', s1, s2);
-        Transition tnd2 = new Transition('b', '2', s1, s4);
+        Transition tnd2 = new Transition('b', '8', s1, s4);
         Transition t2 = new Transition('a', '1', s1, s4);
-        Transition t3 = new Transition('a', '2', s2, s1);
+        Transition t3 = new Transition('a', '3', s2, s1);
         Transition t4 = new Transition('b', '2', s2, s3);
         Transition t5 = new Transition('a', '1', s3, s2);
         Transition t6 = new Transition('b', '2', s3, s1);
@@ -171,101 +141,13 @@ public class Machine {
 
     public static void main(String[] args) {
         // Creating the states;
-
-        Machine M = new Machine();
-
-        State s0 = new State();
-        State s1 = new State();
-        State s2 = new State();
-
-        // Creating the transitions
-
-        Transition t1 = new Transition('a', '0');
-        Transition t2 = new Transition('b', '1');
-        Transition t3 = new Transition('a', '0');
-        Transition t4 = new Transition('b', '1');
-        Transition t5 = new Transition('a', '0');
-        Transition t6 = new Transition('b', '1');
-
-        // Setting the destinations states
-
-        t1.setDestinationState(s1);
-        t2.setDestinationState(s2);
-        t3.setDestinationState(s2);
-        t4.setDestinationState(s0);
-        t5.setDestinationState(s0);
-        t6.setDestinationState(s0);
-
-        // Giving the States their transitions
-
-        s0.setStateTransitions(t1, t2);
-        s1.setStateTransitions(t3, t4);
-        s2.setStateTransitions(t5, t6);
-
-        M.setInitialState(s0);
-        M.setInputAlphabet(Set.of('a', 'b'));
-        M.setOutputAlphabet(Set.of('0', '1'));
-        M.setStates(s0, s1, s2);
-        var ho = M.makeMachineCopy();
-
-        var inputSeq = new ArrayList<Character>();
-        inputSeq.add('a');
-        inputSeq.add('a');
-        inputSeq.add('a');
-        inputSeq.add('b');
-        inputSeq.add('a');
-
-        M.nonDeterministicConsume(inputSeq);
-
-        System.out.println("Second consumption");
-
-        Machine MM = new Machine(); // Think i'm gonna model the machine in the article.
-
-        // 1- States.
-        // 2- Transitions.
-        // 3- Giving states their transitions.
-        // 4- Set the initial state.
-        // 5- Set the input alphabet (Still sus)
-        // 6- Set the output alpahbet (still sus)
-        // 7- Set the machine states
-        // 8- Set the input sequence.
-        // 9- CONSUME !!!
-
-        // 1- States.
-        State q1 = new State(), q2 = new State(), q3 = new State(), q4 = new State();
-        // 2- Transitions
-        Transition tr1 = new Transition('b', '0', q1); // q1
-        Transition tr2 = new Transition('a', '0', q2); // q1
-        Transition tr3 = new Transition('a', '1', q2); // q2
-        Transition tr4 = new Transition('b', '0', q3); // q2
-        Transition tr5 = new Transition('a', '1', q4); // q3
-        Transition tr6 = new Transition('b', '0', q3); // q3
-        Transition tr7 = new Transition('a', '1', q4); // q4
-        Transition tr8 = new Transition('b', '1', q4); // q4
-        // 3- Giving states their transitions.
-        q1.setStateTransitions(tr1, tr2);
-        q2.setStateTransitions(tr3, tr4);
-        q3.setStateTransitions(tr5, tr6);
-        q4.setStateTransitions(tr7, tr8);
-        // 4- Set the initial State
-        MM.setInitialState(q1);
-        // 5- Set the input alphabet (Still sus)
-        MM.setInputAlphabet(Set.of('a', 'b'));
-        // 6- Set the output alphabet (still sus)
-        M.setOutputAlphabet(Set.of('0', '1'));
-        // 7- Set the machine states
-        MM.setStates(q1, q2, q3, q4);
-        // 8- Set the input sequence. and consume
-        MM.nonDeterministicConsume(new ArrayList<>(List.of('a', 'b', 'b', 'a', 'b', 'b', 'b', 'a')));
-
-        // This should give
-        // 0 0 0 1 1 1 1 1
-
-        System.out.println("Actually works, i cant believe it");
-
-        System.out.println(M.toDot());
-
-        System.out.println(ho.toDot());
+        Machine sample = getDefaultMachine();
+        System.out.println(sample.toDot());
+        var input = new ArrayList<Character>();
+        for (Character ch : "ba".toCharArray()) {
+            input.add(ch);
+        }
+        sample.nonDeterministicConsume(input);
     }
 
     /**

d73a97df5b077868b5c5ce48b55c6d9cd0d716b8
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2021-12-03 08:24:25 -0500

    Remove `stateTransitions` and all related things to it in the `State` class.

--- src/main/java/app/oracleextractor/model/State.java
+++ src/main/java/app/oracleextractor/model/State.java
@@ -22,18 +22,15 @@ public class State {
     static int id = 0;
     Machine mach;
     String stateName;
-    ArrayList<Transition> stateTransitions;
 
     /**
      * <code>State</code> constructor.
      */
     public State() {
-        stateTransitions = new ArrayList<>();
         stateName = "q" + id++;
     }
 
     public State(String name) {
-        stateTransitions = new ArrayList<>();
         stateName = name;
     }
 
@@ -47,34 +44,6 @@ public class State {
 
     public void setName(String name) {
         this.stateName = name;
-    }
-
-    public ArrayList<Transition> getStateTransitions() {
-        return stateTransitions;
-    }
-
-    /**
-     * Custom setter function that uses the var-arg syntax to allow easier transition creation/addition to the <code>State</code>.
-     * <br /> <br />
-     * <p>
-     * Might remove the @NotNull annotation since it adds an unnecessary dependency though.
-     *
-     * @param argTransitions The transitions intended for addition into the <code>State</code>.
-     */
-    public void setStateTransitions(Transition... argTransitions) {
-        // Edit: done (prior tasks to be done) : figure out if this is even necessary.
-        //       it is, leave it be, else you get NullPointerException
-        //       which makes sense as the field would be empty
-        stateTransitions = new ArrayList<>();
-        // Have to tell the transitions what their source transition is.
-        for (Transition t : argTransitions) {
-            t.setSourceState(this);
-        }
-        stateTransitions.addAll(Arrays.asList(argTransitions)); // Offered as an alternative syntax by Idea, will test!
-    }
-
-    public void setStateTransitions(ArrayList<Transition> argTransitions) {
-        stateTransitions = argTransitions;
     }
 
     public Machine getMach() {
@@ -93,8 +62,9 @@ public class State {
         Character currTrigger = mach.getNextInputToken();
 
         var applicableTransitions = new ArrayList<Transition>(); // Possible transitions from this state-trigger combo
-        for (Transition t : stateTransitions) { // For every transition this state has ...
-            if (t.isValid() && t.isTriggeredBy(currTrigger)) { // Check if the transition is valid && is the correct response to this character ...
+        for (Transition t : mach.getMachineTransitions()) { // For every transition this state has ...
+            if (t.getSourceState().equals(this) && t.isValid() && t.isTriggeredBy(currTrigger)) {
+                // Check if the transition is valid && is the correct response to this character ...
                 // ... if so, add it to the list of possible transitions
                 applicableTransitions.add(t);
             }

107d89625a0f3be7e564a61c974b464885321b0d
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2021-12-06 14:51:38 -0500

    Add javadoc, overload ST method, add clearing method.
    I am beginning to feel the poor quality of some of my design choices ...

--- src/main/java/app/oracleextractor/model/Trace.java
+++ src/main/java/app/oracleextractor/model/Trace.java
@@ -9,13 +9,31 @@ public class Trace {
         theTrace = new ArrayList<>();
     }
 
-    public void addTrace(State before, State after, Transition transition) {
+    /**
+     * Method to add a <code>StateTransition</code> record to the list of <code>StateTransition</code>s.
+     * @param before The <code>State</code> before taking the <code>Transition</code>
+     * @param after The <code>State</code> after taking the <code>Transition</code>
+     * @param transition The <code>Transition</code> itself.
+     */
+    public void addSTransition(State before, State after, Transition transition) {
         theTrace.add(new StateTransition(before, after, transition));
+    }
+
+    /**
+     * Method to add a <code>StateTransition</code> record to the list of <code>StateTransition</code>s.
+     * @param argStateTransition <code>StateTransition</code> to be added.
+     */
+    public void addSTransition(StateTransition argStateTransition) {
+        theTrace.add(argStateTransition);
     }
 
     public StateTransition getLastChange() {
         int lastElementIndex = theTrace.size() - 1;
         return theTrace.get(lastElementIndex);
+    }
+
+    public void clear(){
+        theTrace.clear();
     }
 
     @Override

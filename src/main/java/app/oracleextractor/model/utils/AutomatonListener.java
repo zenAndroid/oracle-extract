a84d344c265469be4bc1bf7402f7a0a277f41d17
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2021-12-12 21:00:13 -0500

    Populate `AutomatonListener.java`

--- src/main/java/app/oracleextractor/model/utils/AutomatonListener.java
+++ src/main/java/app/oracleextractor/model/utils/AutomatonListener.java
@@ -1,18 +1,44 @@
 package app.oracleextractor.model.utils;
 
-import app.oracleextractor.model.Machine;
 import app.oracleextractor.model.State;
-import app.oracleextractor.reader.*;
+import app.oracleextractor.reader.automatonBaseListener;
+import app.oracleextractor.reader.automatonParser;
+import org.antlr.v4.runtime.ParserRuleContext;
 
-import java.util.ArrayList;
+public class AutomatonListener extends automatonBaseListener {
 
-public class AutomatonListener extends automatonBaseListener{
+    @Override
+    public void enterAutomatonGraph(automatonParser.AutomatonGraphContext ctx) {
+    }
 
-    @Override public void enterInitArrow(automatonParser.InitArrowContext ctx) {
+    @Override
+    public void exitAutomatonGraph(automatonParser.AutomatonGraphContext ctx) {
+    }
+
+    @Override
+    public void enterStatements(automatonParser.StatementsContext ctx) {
+    }
+
+    @Override
+    public void exitStatements(automatonParser.StatementsContext ctx) {
+    }
+
+    @Override
+    public void enterTrigger_output(automatonParser.Trigger_outputContext ctx) {
+    }
+
+    @Override
+    public void exitTrigger_output(automatonParser.Trigger_outputContext ctx) {
+    }
+
+    @Override
+    public void enterInitArrow(automatonParser.InitArrowContext ctx) {
         String initialStateName = ctx.STATE_NAME().getText();
         State initial = State.getState(initialStateName);
         System.out.println(initial);
     }
 
-    @Override public void exitInitArrow(automatonParser.InitArrowContext ctx) { }
+    @Override
+    public void exitInitArrow(automatonParser.InitArrowContext ctx) {
+    }
 }
